4d8295e79f8e0949531148e8c9849b92d3d12f3d
Matthieu Roignant <roignant.matthieu@gmail.com>
2026-06-02 20:50:12 +0200

    Update expert_canape.py

--- expert_canape.py
+++ expert_canape.py
@@ -870,8 +870,7 @@ def show(tables):
         GROUPES_CLASSEMENT = {
             "Classement global": None,
             "Les experts du Canapé": ["Matthieu", "Kévin", "Olivier", "Cédric", "Sébastien", "Pierre"],
-            "Les hasards de dingue": ["Matthieu", "Merguez", "Mélanie", "Sam", "Thifanie"],
-            "Crédit Agricole" : ["Kévin"]
+            "Les hasards de dingue": ["Matthieu", "Merguez", "Mélanie", "Sam", "Thifanie"]
         }
         
         st.subheader(f"Classement {'global' if journee_sel == 'Toutes' else f'jusqu’à la journée {journee_sel}'} – "
